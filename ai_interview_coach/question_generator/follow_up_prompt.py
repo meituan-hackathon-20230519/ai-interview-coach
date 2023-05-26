0316efa52ffb6a0dce4a7b3bb62e00726e3e7e9b
yangxi15 <yangxi15@meituan>
2023-05-26 23:31:51 +0800

    1.让提问精简:concise and short. 2.面试扩展问题和项目经历之间不需要传之前阶段的历史记录 3.面试结束后固定输出面试结束！请点击下一步获取面试评价

--- ai_interview_coach/question_generator/follow_up_prompt.py
+++ ai_interview_coach/question_generator/follow_up_prompt.py
@@ -3,7 +3,7 @@ Given a question template, bullet points about what interviewee's answer should 
 ask a follow-up question to the last human input. Follow the instructions below to generate a question:
 1. First think if last human input is clear and complete. If not, ask for clarification.
 2. Otherwise, use your reasoning and pick bullet points that the interviewee did not cover in his/her answer.
-3. Pick one bullet point from step 2, generate a concise question based on the picked bullet point, the question \
+3. Pick one bullet point from step 2, generate a concise and short question based on the picked bullet point, the question \
 template, and the chat history.
 
 DO NOT explicitly mention you are looking at chat history or resume.
