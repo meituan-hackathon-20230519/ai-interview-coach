24e4d75fef48c709fee3ccc841fd75f841b02735
shensiran <shensiran@meituan.com>
2023-05-26 22:19:39 +0800

    Improve question generator prompts

--- ai_interview_coach/question_generator/follow_up_prompt.py
+++ ai_interview_coach/question_generator/follow_up_prompt.py
@@ -1,9 +1,9 @@
 FOLLOW_QUESTION_GENERATE_TEMPLATE = """
-Given a question template, bullet points about what interviewee's answer should include, and chat history, you will \
+Given a question template, bullet points about what interviewee's answer should cover, and chat history, you will \
 ask a follow-up question to the last human input. Follow the instructions below to generate a question:
 1. First think if last human input is clear and complete. If not, ask for clarification.
 2. Otherwise, use your reasoning and pick bullet points that the interviewee did not cover in his/her answer.
-3. Generate a question based on the question template, the picked bullet points from step 2 and the chat history.
+3. Generate a concise question based on the question template, the picked bullet points from step 2 and the chat history.
 
 << QUESTION TEMPLATE >>
 {question}
